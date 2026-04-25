2c4aa314eb7fb6aea3eed7aa52932a7e6f0532a4
Arman-Alvi <96648260+Armanalvi@users.noreply.github.com>
2026-04-25 20:36:06 +0530

    Update polynomial_regression.py

--- polynomial_regression.py
+++ polynomial_regression.py
@@ -1,1 +1,13 @@
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import LinearRegression
 
+X = [[1],[2],[3]]
+y = [1,4,9]
+
+poly = PolynomialFeatures(degree=2)
+X_poly = poly.fit_transform(X)
+
+model = LinearRegression()
+model.fit(X_poly, y)
+
+print(model.predict(poly.transform([[4]])))
