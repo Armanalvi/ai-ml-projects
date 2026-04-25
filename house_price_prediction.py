d979d0305526a67038f3c7b81c979144ba3f7e93
Arman-Alvi <96648260+Armanalvi@users.noreply.github.com>
2026-04-25 20:59:50 +0530

    Update house_price_prediction.py

--- house_price_prediction.py
+++ house_price_prediction.py
@@ -1,1 +1,9 @@
+from sklearn.linear_model import LinearRegression
 
+X = [[1000],[1500],[2000]]
+y = [50,75,100]
+
+model = LinearRegression()
+model.fit(X,y)
+
+print("Price:", model.predict([[1800]]))
