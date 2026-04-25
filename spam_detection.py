718f8e332680cc5cffdeafedcd900478f8718566
Arman-Alvi <96648260+Armanalvi@users.noreply.github.com>
2026-04-25 21:00:42 +0530

    Update spam_detection.py

--- spam_detection.py
+++ spam_detection.py
@@ -1,1 +1,14 @@
 
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+
+texts = ["free money","hi friend","win prize","hello"]
+labels = [1,0,1,0]
+
+vec = CountVectorizer()
+X = vec.fit_transform(texts)
+
+model = MultinomialNB()
+model.fit(X,labels)
+
+print(model.predict(vec.transform(["free prize"])))
